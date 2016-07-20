8430c0a661cfa47e168e66806cd04bd50390804f
Dave Bouwman <dave@davebouwman.com>
2016-07-20 12:19:42 -0600

    added portal service

---
+++ addon/services/portal-service.js
@@ -0,0 +1,31 @@
+import Ember from 'ember';
+import serviceMixin from '../mixins/service-mixin';
+
+export default Ember.Service.extend(serviceMixin, {
+
+  getPortalRestUrl(){
+    let portalBaseUrl = 'https://www.arcgis.com';
+
+    //check for and use the url configured in the host app
+    if(this.get('hostAppConfig.APP.portalBaseUrl')){
+      portalBaseUrl = this.get('hostAppConfig.APP.portalBaseUrl');
+    }
+    console.debug('Item Service portalBaseUrl: ' + portalBaseUrl);
+    return portalBaseUrl + '/sharing/rest';
+  },
+  /**
+
+
+  /**
+   * Update an existing item
+   * will update the `/data` if the `.text` value is present
+   */
+  update(portal){
+    console.log('Portal Service got update for ' + portal.id);
+    let portalRestUrl = this.getPortalRestUrl();
+    let url = `${portalRestUrl}/content/users/${portal.id}/update?f=json`;
+    return this._post(url, portal);
+  }
+
+
+});
