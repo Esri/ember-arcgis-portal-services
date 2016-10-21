7df5de7ada016ff2b9649461b57fab66a316bdc5
Dave Bouwman <dave@davebouwman.com>
2016-10-20 19:44:26 -0600

    typo in root url

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -55,7 +55,7 @@ module.exports = function (environment) {
 
   if (environment === 'production') {
     ENV.locationType = 'hash';
-    ENV.rootURL = '/ember-arcgis-portal-service/';
+    ENV.rootURL = '/ember-arcgis-portal-services/';
   }
 
   return ENV;
