06a718906cb5915db085049a7b34f999732d3e67
mjuniper <mjuniper@gmail.com>
2018-07-11 12:53:43 -0600

    deletefromdefinition

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 All notable changes to this project will be documented in this file.
 This project adheres to [Semantic Versioning](http://semver.org/).
 
+## Unreleased
+### Added
+- `hosted-service:deleteFromDefinition` method
+
 ## 1.4.0
 ### Added
 - `group-service:inviteUsers` method
--- addon/services/hosted-service.js
+++ addon/services/hosted-service.js
@@ -83,6 +83,24 @@ export default Service.extend(serviceMixin, {
   },
 
   /**
+   * Delete from the service definition
+   */
+  deleteFromDefinition (featureServiceUrl, definition, layerId, portalOpts) {
+    let adminUrl = featureServiceUrl.replace(/\/arcgis\/rest\/services\//i, '/arcgis/rest/admin/services/');
+    let url = `${adminUrl}/deleteFromDefinition?f=json`;
+    if (layerId || layerId === 0) {
+      url = `${adminUrl}/${layerId}/deleteFromDefinition?f=json`;
+    }
+    // since the fs has it's own url we use the requestUrl method
+    return this.requestUrl(url, {
+      method: 'POST',
+      data: {
+        deleteFromDefinition: JSON.stringify(definition)
+      }
+    }, portalOpts);
+  },
+
+  /**
    * Shared logic for POST operations
    */
   _post (urlPath, service, portalOpts) {
